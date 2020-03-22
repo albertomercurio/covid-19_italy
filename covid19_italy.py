da8c799103faf211421ade88b9a0dec215cbfc42
Alberto Mercurio <alberto.mercurio96@gmail.com>
2020-03-22 20:46:11 +0100

    22 Marzo

--- covid19_italy.py
+++ covid19_italy.py
@@ -1,11 +1,12 @@
 import os
 import csv
 import matplotlib.pyplot as plt
-from numpy import exp, linspace, sqrt, diag, diff
+from numpy import exp, linspace, sqrt, diag, diff, mean
 from scipy.optimize import curve_fit
 import datetime
 
 fit_regioni = True #Mettere False se non si vuole eseguire il fit delle singole regioni
+plot_gompertz = True
 
 date = datetime.date.today()
 yesterday = datetime.date.today() - datetime.timedelta(days=1)
@@ -23,6 +24,19 @@ def sigmoid(x, a, b, c):
 def exponential(x, a, b):
     return a*exp(b*x)
 
+def gompertz(x, a, b, c):
+    return a*exp(-b*exp(-c*x))
+
+def r_sqrt(data,data_fitted):
+    if len(data) == len(data_fitted):
+        # ESS = sum([(data_fitted[i]-mean(data))**2 for i in range(len(data_fitted))])
+        RSS = sum([(data_fitted[i]-data[i])**2 for i in range(len(data_fitted))])
+        TSS = sum([(data[i]-mean(data))**2 for i in range(len(data))])
+        return round(1-RSS/TSS,5)
+    else:
+        print("Error: data and data_fitted doesn't have the same dimension.")
+        return 0
+
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     first_row = True
@@ -39,48 +53,67 @@ x = range(len(infetti))
 x2 = range(len(infetti)+delta_t)
 t = linspace(0,len(infetti)+delta_t,100)
 
-lower_1 = [60000,0.01,0]
-# upper_1 = [10000,1,10]
-# lower_2 = [10000,0.001,7]
-# upper_2 = [15000,1,20]
-# lower_3 = [100000,0.001,20]
-upper_3 = [500000,1,100]
-# p0_1 = [6000,0.2,3]
-# p0_2 = [12000,0.2,7]
-# p0_3 = [150000,0.2,40]
-p0 = [130000,0.2,20]
+###################################
+# sigmoid
+###################################
+print("###################################")
+print("Sigmoid")
+print("###################################")
+
+lower = [40000,0.01,0]
+upper = [500000,1,100]
+p0 = [130000,0.2,30]
 print(p0)
 
-# popt, pcov = curve_fit(sigmoid,x[:-2],infetti[:-2],p0=p0,bounds=(lower_1, upper_3),method='trf',
-# max_nfev=50000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="huber")
-# max_infected1 = popt[0]
-# error1 = sqrt(diag(pcov))[0]
-# fitted1 = [sigmoid(i,*popt) for i in t]
-# print(str(popt)+" +- "+str(error1))
-#
-# popt, pcov = curve_fit(sigmoid,x[:-1],infetti[:-1],p0=p0,bounds=(lower_1, upper_3),method='trf',
-# max_nfev=50000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="huber")
-# max_infected2 = popt[0]
-# error2 = sqrt(diag(pcov))[0]
-# fitted2 = [sigmoid(i,*popt) for i in t]
-# print(str(popt)+" +- "+str(error2))
+popt, pcov = curve_fit(sigmoid,x,infetti,p0=p0,bounds=(lower,upper),method='trf',
+max_nfev=100000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="huber")
+max_infected_sig = popt[0]
+error_sigmoid = sqrt(diag(pcov))[0]
+fitted_sig = [sigmoid(i,*popt) for i in t]
+r2_sig = r_sqrt(infetti,[sigmoid(i,*popt) for i in x])
+print(str(popt)+" +- "+str(error_sigmoid))
+print("\n R^2 ="+str(r2_sig)+"\n")
 
-popt, pcov = curve_fit(sigmoid,x,infetti,p0=p0,bounds=(lower_1,upper_3),method='trf',
-max_nfev=100000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="linear")
-max_infected3 = popt[0]
-error3 = sqrt(diag(pcov))[0]
-fitted3 = [sigmoid(i,*popt) for i in t]
-print(str(popt)+" +- "+str(error3))
+###################################
+# gompertz
+###################################
+print("###################################")
+print("Gompertz")
+print("###################################")
+
+lower = [80000,0.01,0.01]
+upper = [1000000,100,0.5]
+p0 = [100000,5,0.1]
+print(p0)
+
+popt, pcov = curve_fit(gompertz,x,infetti,p0=p0,bounds=(lower,upper),method='trf',
+max_nfev=100000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="huber")
+max_infected_gom = popt[0]
+error_gompertz = sqrt(diag(pcov))[0]
+fitted_gom = [gompertz(i,*popt) for i in t]
+r2_gom = r_sqrt(infetti,[gompertz(i,*popt) for i in x])
+print(str(popt)+" +- "+str(error_gompertz))
+print("\n R^2 ="+str(r2_gom)+"\n")
+
+###################################
+# exponential
+###################################
+print("###################################")
+print("Exponential")
+print("###################################")
 
 popt2, pcov2 = curve_fit(exponential,x,infetti,p0=[400,0.2],bounds=([100,0], [10000,2]),method='trf',
-max_nfev=50000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="linear")
+max_nfev=50000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="huber")
 print(popt2)
 exp_fit = [exponential(i,*popt2) for i in t]
+r2_exp = r_sqrt(infetti,[exponential(i,*popt2) for i in x])
+print("\n R^2 ="+str(r2_exp)+"\n")
 
-plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="Epidemia inarrestabile")
+plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="Epidemia inarrestabile ($R^2="+str(r2_exp)+")$")
 # plt.plot(t,fitted1,linestyle="--",lw=0.8,zorder=2,label=bef_yesterday)
 # plt.plot(t,fitted2,linestyle="--",lw=1,zorder=3,label=yesterday)
-plt.plot(t,fitted3,color="red",zorder=4,label=date)
+if plot_gompertz: plt.plot(t,fitted_gom,color="#228B22",zorder=4,label="Gompertz ($R^2="+str(r2_gom)+")$")
+plt.plot(t,fitted_sig,color="red",zorder=4,label="Sigmoide ($R^2="+str(r2_sig)+")$")
 plt.scatter(x,infetti,marker="^",color="black",s=50,zorder=5)
 plt.xlim(0,x[-1]+delta_t2)
 plt.ylim(0,2.5*max(infetti))
@@ -94,10 +127,13 @@ plt.clf()
 plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="$N_{MAX}=\infty$")
 # plt.plot(t,fitted1,linestyle="--",lw=0.8,zorder=1,label="$N_{MAX}="+str(int(max_infected1))+"\pm"+str(round(error1/max_infected1*100,1))+"\%$")
 # plt.plot(t,fitted2,linestyle="--",lw=1,zorder=2,label="$N_{MAX}="+str(int(max_infected2))+"\pm"+str(round(error2/max_infected2*100,1))+"\%$")
-plt.plot(t,fitted3,zorder=3,color="red",label="$N_{MAX}="+str(int(max_infected3))+"\pm"+str(round(error3/max_infected3*100,1))+"\%$")
+if plot_gompertz: plt.plot(t,fitted_gom,zorder=3,color="#228B22",label="$N_{MAX}="+str(int(max_infected_gom))+
+"\pm"+str(round(3*error_gompertz/max_infected_gom*100,1))+"\%$")
+plt.plot(t,fitted_sig,zorder=3,color="red",label="$N_{MAX}="+str(int(max_infected_sig))+
+"\pm"+str(round(3*error_sigmoid/max_infected_sig*100,1))+"\%$")
 plt.scatter(x,infetti,marker="^",color="black",s=40,zorder=4)
 plt.xlim(0,x[-1]+delta_t)
-plt.ylim(0,max(fitted3))
+plt.ylim(0,max(max(fitted_sig),max(fitted_gom)))
 plt.title(str(date)+" in Italia")
 plt.xlabel("Tempo (Giorni dal 24/02/2020)")
 plt.ylabel("Persone infette")
@@ -108,14 +144,17 @@ plt.clf()
 ##################################################################
 
 variazione_infetti = diff(infetti)
-variazione_teorica = diff(fitted3)/(t[1]-t[0])
+variazione_teorica_sig = diff(fitted_sig)/(t[1]-t[0])
+variazione_teorica_gom = diff(fitted_gom)/(t[1]-t[0])
 
 plt.plot(x[:-1],variazione_infetti,lw=1,color="blue",linestyle="-.",zorder=1)
-plt.plot(t[:-1],variazione_teorica,color="red",zorder=2)
+if plot_gompertz: plt.plot(t[:-1],variazione_teorica_gom,color="#228B22",zorder=2)
+plt.plot(t[:-1],variazione_teorica_sig,color="red",zorder=2)
 plt.scatter(x[:-1],variazione_infetti,marker="^",color="black",s=40,zorder=4)
 plt.xlabel("Tempo (Giorni dal 25/02/2020)")
 plt.ylabel("Nuovi infetti")
 plt.xlim(0,t[-1])
+plt.ylim(0,2*max(variazione_infetti))
 plt.title(str(date)+" in Italia")
 plt.savefig("img_italia/nuovi_infetti.png",dpi=200,bbox_inches='tight')
 plt.clf()
@@ -149,8 +188,8 @@ plt.clf()
 ##################################################################
 ##################################################################
 
-max_infected = max_infected3
-err_infected = error3
+max_infected = max(max_infected_sig,max_infected_gom)
+err_infected = max(error_sigmoid,error_gompertz)
 
 url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
 os.system("wget -O data.csv "+url)
@@ -187,24 +226,53 @@ if fit_regioni:
         x2 = range(len(infetti)+delta_t)
         t = linspace(0,len(infetti)+delta_t,100)
 
+        ###################################
+        # sigmoid
+        ###################################
+
         lower_1 = [10,0.01,1]
         upper_3 = [max_infected+3*err_infected,1.2,150]
         p0 = [2000,0.5,20]
 
         popt, pcov = curve_fit(sigmoid,x,infetti,p0=p0,bounds=(lower_1,upper_3),method='trf',
         max_nfev=100000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="linear")
-        max_infected3 = popt[0]
-        error3 = sqrt(diag(pcov))[0]
-        fitted3 = [sigmoid(i,*popt) for i in t]
-        print(str(popt)+" +- "+str(error3))
+        max_infected_sig = popt[0]
+        error_sigmoid = sqrt(diag(pcov))[0]
+        fitted_sig = [sigmoid(i,*popt) for i in t]
+        r2_sig = r_sqrt(infetti,[sigmoid(i,*popt) for i in x])
+        print(str(popt)+" +- "+str(error_sigmoid))
+        print("\n R^2 ="+str(r2_sig)+"\n")
+
+        ###################################
+        # gompertz
+        ###################################
+
+        lower = [10,0.01,0.001]
+        upper = [max_infected+3*err_infected,50,0.5]
+        p0 = [100000,5,0.1]
+
+        popt, pcov = curve_fit(gompertz,x,infetti,p0=p0,bounds=(lower,upper),method='trf',
+        max_nfev=100000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="linear")
+        max_infected_gom = popt[0]
+        error_gompertz = sqrt(diag(pcov))[0]
+        fitted_gom = [gompertz(i,*popt) for i in t]
+        r2_gom = r_sqrt(infetti,[gompertz(i,*popt) for i in x])
+        print(str(popt)+" +- "+str(error_gompertz))
+        print("\n R^2 ="+str(r2_gom)+"\n")
         print("############")
+
+        ###################################
+        # exponential
+        ###################################
 
         popt, pcov = curve_fit(exponential,x,infetti,p0=[400,0.2],bounds=([0.001,0], [10000,2]),method='trf',
         max_nfev=50000,xtol=1e-15,gtol=1e-15,ftol=1e-15,jac="3-point",loss="linear")
         exp_fit = [exponential(i,*popt) for i in t]
+        r2_exp = r_sqrt(infetti,[exponential(i,*popt) for i in x])
 
-        plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="Epidemia inarrestabile")
-        plt.plot(t,fitted3,color="red",zorder=4,label=date)
+        plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="Epidemia inarrestabile ($R^2="+str(r2_exp)+")$")
+        if plot_gompertz: plt.plot(t,fitted_gom,color="#228B22",zorder=4,label="Gompertz ($R^2="+str(r2_gom)+")$")
+        plt.plot(t,fitted_sig,color="red",zorder=4,label="Sigmoid ($R^2="+str(r2_sig)+")$")
         plt.scatter(x,infetti,marker="^",color="black",s=50,zorder=5)
         plt.xlim(0,x[-1]+delta_t2)
         plt.ylim(0,2.5*max(infetti))
@@ -216,10 +284,13 @@ if fit_regioni:
         plt.clf()
 
         plt.plot(t,exp_fit,linestyle="-.",zorder=1,label="$N_{MAX}=\infty$")
-        plt.plot(t,fitted3,zorder=3,color="red",label="$N_{MAX}="+str(int(max_infected3))+"\pm"+str(round(error3/max_infected3*100,1))+"\%$")
+        if plot_gompertz: plt.plot(t,fitted_gom,zorder=3,color="#228B22",label="$N_{MAX}="+str(int(max_infected_gom))+
+            "\pm"+str(round(3*error_gompertz/max_infected_gom*100,1))+"\%$")
+        plt.plot(t,fitted_sig,zorder=3,color="red",label="$N_{MAX}="+str(int(max_infected_sig))+
+        "\pm"+str(round(3*error_sigmoid/max_infected_sig*100,1))+"\%$")
         plt.scatter(x,infetti,marker="^",color="black",s=40,zorder=4)
         plt.xlim(0,x[-1]+delta_t)
-        plt.ylim(0,max(fitted3))
+        plt.ylim(0,max(max(fitted_sig),max(fitted_gom)))
         plt.title(str(date)+" in "+regione)
         plt.xlabel("Tempo (Giorni dal 24/02/2020)")
         plt.ylabel("Persone infette")
@@ -230,13 +301,16 @@ if fit_regioni:
         ##################################################################
 
         variazione_infetti = diff(infetti)
-        variazione_teorica = diff(fitted3)/(t[1]-t[0])
+        variazione_teorica_sig = diff(fitted_sig)/(t[1]-t[0])
+        variazione_teorica_gom = diff(fitted_gom)/(t[1]-t[0])
 
         plt.plot(x[:-1],variazione_infetti,lw=1,color="blue",linestyle="-.",zorder=1)
-        plt.plot(t[:-1],variazione_teorica,color="red",zorder=2)
+        if plot_gompertz: plt.plot(t[:-1],variazione_teorica_gom,color="#228B22",zorder=2)
+        plt.plot(t[:-1],variazione_teorica_sig,color="red",zorder=2)
         plt.scatter(x[:-1],variazione_infetti,marker="^",color="black",s=40,zorder=4)
         plt.xlabel("Tempo (Giorni dal 25/02/2020)")
         plt.ylabel("Nuovi infetti")
+        plt.ylim(0,2*max(variazione_infetti))
         plt.title(str(date)+" in "+regione)
         plt.savefig(directory+"/"+"nuovi_infetti.png",dpi=200,bbox_inches='tight')
         plt.clf()
